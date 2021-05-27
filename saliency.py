f5d9dfe45415d7f827b4bb1c998f97a65c3926f5
rehrev <gumityolcu@gmail.com>
2021-05-28 02:06:00 +0300

    Remove unneeded training code from saliency.py, fixes crash at the end of an episode.

--- saliency.py
+++ saliency.py
@@ -47,8 +47,6 @@ if __name__ == "__main__":
     last_100_ep_reward = deque(maxlen=100)  # Last 100 episode rewards
     total_step = 1  # Cumulative sum of all steps in episodes
     for episode in range(startEpisode, MAX_EPISODE):
-
-        startTime = time.time()  # Keep time
         state = environment.reset()  # Reset env
         atariimg = state
         state = agent.preProcess(state)  # Process image
@@ -57,17 +55,10 @@ if __name__ == "__main__":
         # We stack frames like 4 channel image
         state = np.stack((state, state, state, state))
 
-        total_max_q_val = 0  # Total max q vals
-        total_reward = 0  # Total reward for each episode
-        total_loss = 0  # Total loss for each episode
         for step in range(MAX_STEP):
             # Select and perform an action
             action = agent.act(state)  # Act
             environment.render()
-            if step>START_VIEW:
-                pass
-                #plt.imshow(atariimg)
-                #plt.show()
             possaliency, negsaliency=agent.online_model.getSaliencyMap(state, 'advantage', action=action, PosNeg=True)
             possaliency=possaliency[0].cpu() #Use the saliency map for the most recent image
             negsaliency=negsaliency[0].cpu()
@@ -86,12 +77,6 @@ if __name__ == "__main__":
                 img=cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
                 cv2.imshow("Display window", img)
                 cv2.waitKey(60)
-            #for i in range(4):
-                #plt.subplot(1,4,i+1)
-                #img=torch.cat([negsaliency[i].unsqueeze(0).cpu(), possaliency[i].unsqueeze(0).cpu(), torch.tensor(state[0]).unsqueeze(0)])#torch.zeros(possaliency[i].unsqueeze(0).size())])
-                #plt.imshow(img.transpose(0,2).transpose(0,1))
-                #plt.show()
-            #    exit()
 
 
             next_state, reward, done, info = environment.step(action)  # Observe
@@ -103,31 +88,10 @@ if __name__ == "__main__":
             # We stack frames like 4 channel image
             next_state = np.stack((next_state, state[0], state[1], state[2]))
 
-            # Store the transition in memory
-            agent.storeResults(state, action, reward, next_state, done)  # Store to mem
-
             # Move to the next state
             state = next_state  # Update state
 
-            loss, max_q_val = [0, 0]
 
-            total_loss += loss
-            total_max_q_val += max_q_val
-            total_reward += reward
-            total_step += 1
-            if total_step % 1000 == 0:
-                agent.adaptiveEpsilon()  # Decrase epsilon
 
             if done:  # Episode completed
-                currentTime = time.time()  # Keep current time
-                time_passed = currentTime - startTime  # Find episode duration
-                current_time_format = time.strftime("%H:%M:%S", time.gmtime())  # Get current dateTime as HH:MM:SS
-                epsilonDict = {'epsilon': agent.epsilon}  # Create epsilon dict to save model as file
-
-                outStr = "Episode:{} Time:{} Reward:{:.2f} Loss:{:.2f} Last_100_Avg_Rew:{:.3f} Avg_Max_Q:{:.3f} Epsilon:{:.2f} Duration:{:.2f} Step:{} CStep:{}".format(
-                    episode, current_time_format, total_reward, total_loss, np.mean(last_100_ep_reward), avg_max_q_val,
-                    agent.epsilon, time_passed, step, total_step
-                )
-
-                print(outStr)
                 break
