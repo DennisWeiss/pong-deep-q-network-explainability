44caee3ae636d51d59e2bbd58afeda162cb201d1
Dennis Weiss <weiss.dennis98@yahoo.de>
2021-06-01 21:16:35 +0200

    Adding action tree

--- pong.py
+++ pong.py
@@ -45,7 +45,7 @@ class DuelCNN(nn.Module):
     """
     def __init__(self, h, w, output_size):
         super(DuelCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=4,  out_channels=32, kernel_size=8, stride=4)
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4)
         self.bn1 = nn.BatchNorm2d(32)
         convw, convh = self.conv2d_size_calc(w, h, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
